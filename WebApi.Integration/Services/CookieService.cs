0060d4d28bf2a735d227df1b6ae6c3cd973ce6ce
My git <notamail@ya.ru>
2023-06-14 16:18:04 +0300

    Add test for /Auth/Login cookie service

--- WebApi.Integration/Services/CookieService.cs
+++ WebApi.Integration/Services/CookieService.cs
@@ -23,8 +23,20 @@ public class CookieService
         var response = await GetCookieInternalAsync(name, password);
         return response.Headers.FirstOrDefault(h=> h.Key == "Set-Cookie").Value.ToList().First();
     }
-    
-    public async Task<HttpResponseMessage> GetCookieInternalAsync(string name, string password)
+     
+	public async Task<(bool, string)> GetCookieWithResultAsync(string name, string password)
+	{
+        string cookie = null;
+        var response = await GetCookieInternalAsync(name, password);
+        if (response.IsSuccessStatusCode)
+        {
+            var firstHeader = response.Headers.FirstOrDefault(h => h.Key == "Set-Cookie").Value;
+			cookie = firstHeader?.First();
+		}
+        return (cookie != null, cookie);
+	}
+
+	public async Task<HttpResponseMessage> GetCookieInternalAsync(string name, string password)
     {
         return await _cookieAuthApiClient.GetAuthCookieAsync(name, password);
     }
---
+++ WebApi.Integration/Tests/CookieAuthTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebApi.Integration.Services;
+using Xunit;
+
+namespace WebApi.Integration.Tests
+{
+	public class CookieAuthTest
+	{
+		private readonly CookieService _cookieService = new();
+
+		[Fact]
+		public async Task IfLoginAndPasswordAreCorrect_CookieShouldBeReceived()
+		{
+			//Arrange
+			string name = "admin";
+			string password = "admin";
+
+			//Act				
+			var results = await _cookieService.GetCookieWithResultAsync(name, password);
+
+			//Assert
+			Assert.True(results.Item1);
+			var cookie = results.Item2;
+			Assert.NotNull(cookie);
+		}
+
+		[Fact]
+		public async Task IfPasswordIsIncorrect_CookieShouldNotBeReceived()
+		{
+			//Arrange
+			string name = "admin";
+			string password = Guid.NewGuid().ToString();
+
+			//Act
+			var results = await _cookieService.GetCookieWithResultAsync(name, password);
+
+			//Assert
+			Assert.False(results.Item1);
+			var cookie = results.Item2;
+			Assert.Null(cookie);
+		}
+	}
+}
